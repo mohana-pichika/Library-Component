28f430838c2b4026a4813f203fd29aef48b55183
mohana-pichika <pichika.1@buckeyemail.osu.edu>
2026-04-24 21:12:51 -0400

    enhanced interface

--- src/Librarykernal.java
+++ src/Librarykernal.java
@@ -1,4 +1,10 @@
 import components.standard.Standard;
+import components.map.Map;
+import components.map.Map1L;
+import components.sequence.Sequence;
+import components.sequence.Sequence1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
 
 /**
  * Kernal Interface.
